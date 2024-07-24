bcf891210ce3bd283b77f0338e95c0e40be5b265
Marlon Knupp <marlonjcc23@gmail.com>
2024-07-23 23:13:39 -0300

    ....

--- Main.py
+++ Main.py
@@ -13,3 +13,4 @@ fonte_cabecalho = font.Font(family="Garamond", size=24, weight="bold")
 rotulo_cabecalho =  tk.Label(janela, text="Meu App de tarefas", font=fonte_cabecalho, bg="#F0f0f0", fg="#333").pack(pady=20)
 janela.mainloop()
 
+#######3
