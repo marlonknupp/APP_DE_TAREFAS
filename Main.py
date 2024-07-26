5496673bb15ac807b8bbba9d300ab66e6840f76d
Marlon Knupp <marlonjcc23@gmail.com>
2024-07-26 00:58:25 -0300

    atualizando

--- Main.py
+++ Main.py
@@ -14,7 +14,10 @@ rotulo_cabecalho =  tk.Label(janela, text="Meu App de tarefas", font=fonte_cabec
 
 frame = tk.Frame(janela,bg="#F0F0F0").pack(pady=10)
 
-entrada_tarefa = tk.Entry(frame,font=("Garamond", 14), relief=tk.FLAT, bg = "white", fg="grey", width)
+entrada_tarefa = tk.Entry(frame,font=("Garamond", 14), relief=tk.FLAT, bg="white", fg="grey", width=30)
+entrada_tarefa.pack(side=tk.LEFT,padx=10)
 
+botao_adicionar = tk.Button(frame, text="adicionar tarefa", bg="#4CAF50", fg="white", height=1, width=15,font=("Roboto", 11), relief=tk.FLAT)
+botao_adicionar.pack(side=tk.LEFT, padx =10)
 janela.mainloop()
 
